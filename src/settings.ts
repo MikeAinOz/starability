3dfafce16ef5596ada2d987b85206b3f36e4c9b6
MikeAinOz <mike@mikeallen.com.au>
2020-06-26 15:06:26 +1000

    Removed Datapoint Capability

--- src/settings.ts
+++ src/settings.ts
@@ -29,22 +29,9 @@
     import DataViewObjectsParser = dataViewObjectsParser.DataViewObjectsParser;
 
     export class VisualSettings extends DataViewObjectsParser {
-      public dataPoint: dataPointSettings = new dataPointSettings();
       public url: postUrlSettings = new postUrlSettings();
       }
 
-    export class dataPointSettings {
-     // Default color
-      public defaultColor: string = "";
-     // Show all
-      public showAllDataPoints: boolean = true;
-     // Fill
-      public fill: string = "";
-     // Color saturation
-      public fillRule: string = "";
-     // Text Size
-      public fontSize: number = 12;
-     }
      export class postUrlSettings {
       // Target Url
       public targetUrl: string = "https://";
